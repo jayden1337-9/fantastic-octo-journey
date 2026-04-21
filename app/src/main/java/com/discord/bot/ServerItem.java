ce2c7d8c008b4a9115bc6efb5cc4ad3f1ebd3c9b
jayden1337-9 <jayden13372000@gmail.com>
2026-04-21 20:18:50 +0200

    Update ServerItem.java

--- app/src/main/java/com/discord/bot/ServerItem.java
+++ app/src/main/java/com/discord/bot/ServerItem.java
@@ -1,9 +1,16 @@
 package com.discord.bot;
 
 public class ServerItem {
-    public String name;
 
-    public ServerItem(String name) {
+    public String id;
+    public String name;
+    public String icon;
+    public String type;
+
+    public ServerItem(String id, String name, String icon, String type) {
+        this.id = id;
         this.name = name;
+        this.icon = icon;
+        this.type = type;
     }
 }
